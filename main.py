b5c2288787577a08cdcb39eae23ac471afc8ec54
lperafan <lperafan@unomaha.edu>
2025-04-26 02:54:07 +0000

    done

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ fats = []
 for item in report:
     description = item["Description"]
     fat = item["Data"]["Fat"]["Total Lipid"]
-    if "Milk" in description.lower() and fat > 15:
+    if 60< fat < 90:
         descriptions.append(description)
         fats.append(fat)
 
@@ -25,7 +25,21 @@ df = df.sort_values(by = "Fat", ascending = True)
 print(df)
 
 df.plot(kind = 'bar', x = 'Description', y = 'Fat')
-plt.xlabel('Milk Description')
+plt.xlabel('Food Description')
 plt.ylabel('Total Fat (g)')
-plt.title('Fat Content of Milks')
+plt.title('Fat Content of Food')
 plt.savefig("output")
+
+print()
+print("Graph Explation:")
+print("My graph shows the fat content in different foods.", "The x-axis lists the food descriptions, while the y-axis indicates the amount of fat in grams.")
+
+print()
+print("Insights from the Data:")
+print("The graph shows which foods have the most fat in them. For instance, Oil or table fat, NFS and Salt pork, contain really high fat percentages.")
+print("The fat content varies quite a bit, starting from around 60 grams going up to more than 87 grams.")
+
+print()
+print("Observations and Trends:")
+print("Nuts like hazelnuts and pecans are often listed as high-fat foods, which makes sense given what we know about their nutritional content.")
+print("The grouping of certain food categories could suggest that some food groups naturally have higher fat content.")
